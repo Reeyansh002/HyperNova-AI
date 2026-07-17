22c92039d511caf36ecd494cb870d30777189539
Reeyansh002 <rmotivation990@gmail.com>
2026-07-18 01:45:35 +0530

    Initial project setup with React frontend and Express backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use (express.json());
+
+app.get("/",(req,res) =>{
+    res.send("HyperNova backend is running");
+});
+
+app.listen(5000 ,() =>{
+    console.log("Server is running on post : 5000")
+});
+
